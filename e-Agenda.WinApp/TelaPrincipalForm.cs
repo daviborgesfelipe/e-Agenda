0f6ec5b8a471cfb26872bca2ccdfc2dd33f9576b
Davi Borges <davifelipe_300@hotmail.com>
2023-05-20 14:47:59 -0300

    cria um metodo para habilitar os botoes inser editar e excluir, e adiciona no metodo de click dos menus

--- e-Agenda.WinApp/TelaPrincipalForm.cs
+++ e-Agenda.WinApp/TelaPrincipalForm.cs
@@ -1,7 +1,9 @@
 using e_Agenda.WinApp.Compartilhado;
+using e_Agenda.WinApp.ModuloCompromiso;
 using e_Agenda.WinApp.ModuloCompromisso;
 using e_Agenda.WinApp.ModuloContato;
 using e_Agenda.WinApp.ModuloTarefa;
+using System.Globalization;
 
 namespace e_Agenda.WinApp
 {
@@ -14,12 +16,38 @@ namespace e_Agenda.WinApp
         public TelaPrincipalForm()
         {
             InitializeComponent();
+
+            Contato contato1 = new Contato("Davi", "3241-3078", "davi@gmail.com", "Desenvolvedor", "NDD");
+            Contato contato2 = new Contato("Thiagao", "3345-7833", "thiagao@gmail.com", "Professor Desenvolvedor", "Academia do Programador");
+            Contato contato3 = new Contato("Caio", "3111-9844", "caio@gmail.com", "Desenvolvedor", "NDD");
+            contato1.id = 30;
+            contato2.id = 31;
+            contato3.id = 32;
+            repositorioContato.Inserir(contato1);
+            repositorioContato.Inserir(contato2);
+            repositorioContato.Inserir(contato3);
+
+            Compromisso compromisso1 = new Compromisso("Aula", "Online", Convert.ToDateTime("25/05/2023").Date ,Convert.ToDateTime("11:00"), Convert.ToDateTime("12:00"));
+            Compromisso compromisso2 = new Compromisso("Estudar", "Presencial", Convert.ToDateTime("18/05/2023").Date, Convert.ToDateTime("08:00", CultureInfo.InvariantCulture), Convert.ToDateTime("22:00", CultureInfo.InvariantCulture));
+            Compromisso compromisso3 = new Compromisso("Trabalho", "Online", Convert.ToDateTime("13/06/2023").Date, Convert.ToDateTime("12:00", CultureInfo.InvariantCulture), Convert.ToDateTime("23:00", CultureInfo.InvariantCulture));
+            compromisso1.id = 14;
+            compromisso1.contato = contato2;
+            compromisso2.id = 24;
+            compromisso2.contato = contato1;
+            compromisso3.id = 34;
+            compromisso3.contato = contato3;
+            repositorioCompromisso.Inserir(compromisso1);
+            repositorioCompromisso.Inserir(compromisso2);
+            repositorioCompromisso.Inserir(compromisso3);
         }
         private void compromissosMenuItem_Click(object sender, EventArgs e)
         {
-            controlador = new ControladorCompromisso(repositorioCompromisso);
+            controlador = new ControladorCompromisso(repositorioCompromisso, repositorioContato);
 
             ConfigurarTelaPrincipal(controlador);
+
+            HabilitarBotoesInserirEditarExcluir();
+
         }
 
         private void contatosMenuItem_Click(object sender, EventArgs e)
@@ -27,6 +55,8 @@ namespace e_Agenda.WinApp
             controlador = new ControladorContato(repositorioContato);
 
             ConfigurarTelaPrincipal(controlador);
+
+            HabilitarBotoesInserirEditarExcluir();
         }
 
         private void tarefasMenuItem_Click(object sender, EventArgs e)
@@ -34,6 +64,17 @@ namespace e_Agenda.WinApp
             controlador = new ControladorTarefa();
 
             ConfigurarTelaPrincipal(controlador);
+
+            HabilitarBotoesInserirEditarExcluir();
+
+        }
+
+        private void HabilitarBotoesInserirEditarExcluir()
+        {
+            btnEditar.Enabled = true;
+            btnExcluir.Enabled = true;
+            btnInserir.Enabled = true;
+            btnFiltrar.Enabled = true;
         }
 
         private void ConfigurarTelaPrincipal(ControladorBase controladorBase)
@@ -61,6 +102,7 @@ namespace e_Agenda.WinApp
             btnInserir.ToolTipText = controlador.ToolTipInserir;
             btnEditar.ToolTipText = controlador.ToolTipEditar;
             btnExcluir.ToolTipText = controlador.ToolTipExcluir;
+            btnFiltrar.ToolTipText = controlador.ToolTipFiltrar;
         }
 
         private void btnInserir_Click(object sender, EventArgs e)
@@ -78,5 +120,9 @@ namespace e_Agenda.WinApp
             controlador.Excluir();
         }
 
+        private void btnFiltrar_Click(object sender, EventArgs e)
+        {
+            controlador.Filtrar();
+        }
     }
 }
