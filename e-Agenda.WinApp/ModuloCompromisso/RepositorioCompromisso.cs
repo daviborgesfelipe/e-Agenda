4010f57499a0bba6e3cbd23bb7d1d91dd78e2b64
Davi Borges <davifelipe_300@hotmail.com>
2023-05-22 23:15:37 -0300

    utilizar os beneficios da heranca do repositorio base

--- e-Agenda.WinApp/ModuloCompromisso/RepositorioCompromisso.cs
+++ e-Agenda.WinApp/ModuloCompromisso/RepositorioCompromisso.cs
@@ -1,46 +1,16 @@
-﻿using e_Agenda.WinApp.ModuloCompromiso;
-using e_Agenda.WinApp.ModuloContato;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using e_Agenda.WinApp.Compartilhado;
+using e_Agenda.WinApp.ModuloCompromiso;
 
 namespace e_Agenda.WinApp.ModuloCompromisso
 {
-    public class RepositorioCompromisso
+    public class RepositorioCompromisso : RepositorioBase<Compromisso>
     {
-        List<Compromisso> compromissos = new List<Compromisso>();
-        private static int contador;
-        internal void Inserir(Compromisso compromisso)
-        {
-            contador++;
-            compromisso.id = contador;
-            compromissos.Add(compromisso);
-        }
+        List<Compromisso> compromissos;
 
-        internal List<Compromisso> SelecionarTodos()
+        public RepositorioCompromisso(List<Compromisso> compromissos)
         {
-            return compromissos;
-        }
-        public void Editar(Compromisso compromisso)
-        {
-            Compromisso compromissoSelecionado = SelecionarPorId(compromisso.id);
-
-            compromissoSelecionado.assunto = compromisso.assunto;
-            compromissoSelecionado.local = compromisso.local;
-            compromissoSelecionado.contato = compromisso.contato;
-            compromissoSelecionado.dataCompromisso = compromisso.dataCompromisso;
-            compromissoSelecionado.dataInicio = compromisso.dataInicio;
-            compromissoSelecionado.dataTermino = compromisso.dataTermino;
-        }
-        private Compromisso SelecionarPorId(int id)
-        {
-            return compromissos.FirstOrDefault(x => x.id == id);
-        }
-        public void Excluir(Compromisso compromisso)
-        {
-            compromissos.Remove(compromisso);
+            this.listaRegistros = compromissos;
+            compromissos = listaRegistros;
         }
 
         internal List<Compromisso> SelecionarCompromissosFuturos(DateTime dataInicial, DateTime dataFinal)
@@ -48,7 +18,6 @@ namespace e_Agenda.WinApp.ModuloCompromisso
             return compromissos.Where(c => c.dataCompromisso >= dataInicial && c.dataCompromisso <= dataFinal).ToList();
 
         }
-
         internal List<Compromisso> SelecionarCompromissosPassados(DateTime now)
         {
             return compromissos.Where(c => c.dataCompromisso < now).ToList();
