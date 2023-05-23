12b635709120ba375bfdc740437c524bf920fe72
Davi Borges <davifelipe_300@hotmail.com>
2023-05-22 23:20:24 -0300

    adiciona os metodos atualizar informacoes e validar

--- e-Agenda.WinApp/ModuloCompromisso/Compromisso.cs
+++ e-Agenda.WinApp/ModuloCompromisso/Compromisso.cs
@@ -1,14 +1,9 @@
 ﻿using e_Agenda.WinApp.Compartilhado;
 using e_Agenda.WinApp.ModuloContato;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace e_Agenda.WinApp.ModuloCompromiso
 {
-    public class Compromisso : EntidadeBase
+    public class Compromisso : EntidadeBase<Compromisso>
     {
         public Contato contato;
         public string assunto;
@@ -30,9 +25,45 @@ namespace e_Agenda.WinApp.ModuloCompromiso
             this.contatoList = new List<Contato>();
         }
 
+        public override void AtualizarInformacoes(Compromisso registroAtualizado)
+        {
+            this.assunto = registroAtualizado.assunto;
+            this.local = registroAtualizado.local;
+            this.contato = registroAtualizado.contato;
+            this.dataCompromisso = registroAtualizado.dataCompromisso;
+            this.dataInicio = registroAtualizado.dataInicio;
+            this.dataTermino = registroAtualizado.dataTermino;
+        }
+
         public override string ToString()
         {
-            return "Id: " + id + ", " + assunto + ", Contato: " + contato.nome + ", Local" + local + ", Data: " + dataCompromisso.ToShortDateString() + ", Inicio: " + dataInicio.ToShortTimeString() + ", Termino: " + dataTermino.ToShortTimeString();
+            return "Id: " + id + "  - Assunto: " + assunto + "  - Contato: " + contato.nome + "  - Local: " + local + "  - Data Compromisso: " + dataCompromisso.ToShortDateString() + "  - Horario Inicio: " + dataInicio.ToShortTimeString() + "  - Horario Termino: " + dataTermino.ToShortTimeString();
+        }
+
+        public override List<string> Validar()
+        {
+            List<string> erros = new List<string>();
+            if (assunto == null)
+            {
+                erros.Add("O campo \"assunto\" é obrigatorio");
+            }
+            if (dataInicio > dataTermino)
+            {
+                erros.Add("O campo \"dataIncio\" data do inicio esta no futuro da data de termino");
+            }
+            if (dataInicio == null)
+            {
+                erros.Add("O campo \"dataIncio\" é obrigatoria");
+            }
+            if (dataTermino > dataInicio)
+            {
+                erros.Add("O campo \"dataTermino\" data de termino esta do passado da data de inicio");
+            }
+            if (dataTermino == null)
+            {
+                erros.Add("O campo \"dataTermino\" é obrigatorio");
+            }
+            return erros;
         }
     }
 }
