60d8db8af5c3ad886abccbfe75e68a2bd59e2903
Davi Borges <davifelipe_300@hotmail.com>
2023-05-22 23:10:24 -0300

    cria uma interface para o repositorio base

---
+++ e-Agenda.WinApp/Compartilhado/Interfaces/IRepositorioBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_Agenda.WinApp.Compartilhado.Interfaces
+{
+    public interface IRepositorioBase<IEntidadeBase>
+    {
+        public void Inserir(IEntidadeBase registro);
+        public void Editar(int id, IEntidadeBase registroAtualizado);
+        public void Editar(IEntidadeBase registroSelecionado, IEntidadeBase registroAtualizado);
+        public void Excluir(int id);
+        public void Excluir(IEntidadeBase registroSelecionado);
+        public IEntidadeBase SelecionarPorId(int id);
+        public List<IEntidadeBase> SelecionarTodos();
+        public bool TemRegistros();
+    }
+}
