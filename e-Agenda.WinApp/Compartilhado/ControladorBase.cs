d53299020f425c5755830ed49b00c16ad34a8005
Davi Borges <davifelipe_300@hotmail.com>
2023-05-23 12:11:54 -0300

    muda filtrar para virtual e remove linhas em branco

--- e-Agenda.WinApp/Compartilhado/ControladorBase.cs
+++ e-Agenda.WinApp/Compartilhado/ControladorBase.cs
@@ -1,24 +1,19 @@
-﻿namespace e_Agenda.WinApp.Compartilhado
+﻿using e_Agenda.WinApp.Compartilhado.Interfaces;
+
+namespace e_Agenda.WinApp.Compartilhado
 {
-    public abstract class ControladorBase
+    public abstract class ControladorBase : IControladorBase
     {
         public abstract string ToolTipInserir { get; }
-
         public abstract string ToolTipEditar { get; }
-
         public abstract string ToolTipExcluir { get; }
-        public abstract string ToolTipFiltrar { get; }
+        public virtual string ToolTipFiltrar { get; }
 
+        public virtual void Filtrar() { }
         public abstract void Inserir();
-
         public abstract void Editar();
-
         public abstract void Excluir();
-
-        public abstract void Filtrar();
-
         public abstract UserControl ObterListagem();
-
         public abstract string ObterTipoCadastro();
     }
 
