6fb727cc49fc579aeb1cf68b67471fa3f2c4855e
Davi Borges <davifelipe_300@hotmail.com>
2023-05-23 12:10:38 -0300

    adiciona interface do controlador base

---
+++ e-Agenda.WinApp/Compartilhado/Interfaces/IControladorBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_Agenda.WinApp.Compartilhado.Interfaces
+{
+    public interface IControladorBase
+    {
+        public void Filtrar();
+        public void Inserir();
+        public void Editar();
+        public void Excluir();
+        public UserControl ObterListagem();
+        public string ObterTipoCadastro();
+    }
+}
