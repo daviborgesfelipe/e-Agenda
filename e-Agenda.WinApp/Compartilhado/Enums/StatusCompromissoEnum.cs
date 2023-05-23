df8bff9addd00a75f994deece933c362ead89d7c
Davi Borges <davifelipe_300@hotmail.com>
2023-05-22 23:09:14 -0300

    muda local da pasta

---
+++ e-Agenda.WinApp/Compartilhado/Enums/StatusCompromissoEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_Agenda.WinApp.ModuloCompromisso
+{
+    public enum StatusCompromissoEnum
+    {
+        Todos,
+        Passados,
+        Futuros
+    }
+}
