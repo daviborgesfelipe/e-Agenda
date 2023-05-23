17e33b7ee52386940cb441d0f9d87fd704dfbc37
Davi Borges <davifelipe_300@hotmail.com>
2023-05-23 12:13:24 -0300

    adiciona a prop tipo local

--- e-Agenda.WinApp/ModuloCompromisso/Compromisso.cs
+++ e-Agenda.WinApp/ModuloCompromisso/Compromisso.cs
@@ -1,4 +1,5 @@
 ﻿using e_Agenda.WinApp.Compartilhado;
+using e_Agenda.WinApp.ModuloCompromisso.Enums;
 using e_Agenda.WinApp.ModuloContato;
 
 namespace e_Agenda.WinApp.ModuloCompromiso
@@ -7,37 +8,55 @@ namespace e_Agenda.WinApp.ModuloCompromiso
     {
         public Contato contato;
         public string assunto;
-        public string local;
+        public string localRemoto;
+        public string localPresencial;
         public DateTime dataCompromisso;
         public DateTime dataInicio;
         public DateTime dataTermino;
+        public TipoCompromissoEnum tipoLocal;
 
         public List<Contato> contatoList;
 
-        public Compromisso(string assunto, string local, DateTime dataCompromisso, DateTime dataInicio, DateTime dataTermino)
+        public Compromisso(string assunto, string local, DateTime dataCompromisso, DateTime dataInicio, DateTime dataTermino, TipoCompromissoEnum tipoLocal)
         {
             this.assunto = assunto;
-            this.local = local;
             this.dataCompromisso = dataCompromisso;
             this.dataInicio = dataInicio;
             this.dataTermino = dataTermino;
+            this.tipoLocal = tipoLocal;
 
+            if (tipoLocal == TipoCompromissoEnum.Remoto)
+            {
+                this.localRemoto = local;
+            }
+            else
+            {
+                this.localPresencial = local;
+            }
             this.contatoList = new List<Contato>();
         }
 
         public override void AtualizarInformacoes(Compromisso registroAtualizado)
         {
             this.assunto = registroAtualizado.assunto;
-            this.local = registroAtualizado.local;
             this.contato = registroAtualizado.contato;
             this.dataCompromisso = registroAtualizado.dataCompromisso;
             this.dataInicio = registroAtualizado.dataInicio;
             this.dataTermino = registroAtualizado.dataTermino;
+            this.contato = registroAtualizado.contato;
+            if (this.tipoLocal == TipoCompromissoEnum.Remoto)
+            {
+                this.localRemoto = registroAtualizado.localRemoto;
+            }
+            else
+            {
+                this.localPresencial = registroAtualizado.localPresencial;
+            }
         }
 
         public override string ToString()
         {
-            return "Id: " + id + "  - Assunto: " + assunto + "  - Contato: " + contato.nome + "  - Local: " + local + "  - Data Compromisso: " + dataCompromisso.ToShortDateString() + "  - Horario Inicio: " + dataInicio.ToShortTimeString() + "  - Horario Termino: " + dataTermino.ToShortTimeString();
+            return "Id: " + id + "  - Assunto: " + assunto + "  - Contato: " + contato.nome + "  - Tipo Local: " + tipoLocal.ToString()  + "  - Data Compromisso: " + dataCompromisso.ToShortDateString() + "  - Horario Inicio: " + dataInicio.ToShortTimeString() + "  - Horario Termino: " + dataTermino.ToShortTimeString();
         }
 
         public override List<string> Validar()
