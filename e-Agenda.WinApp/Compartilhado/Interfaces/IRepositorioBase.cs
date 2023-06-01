74b1aba1b4d123d6116223b9f038703579b5c467
Davi Borges <davifelipe_300@hotmail.com>
2023-06-01 13:19:19 -0300

    remove using

--- e-Agenda.WinApp/Compartilhado/Interfaces/IRepositorioBase.cs
+++ e-Agenda.WinApp/Compartilhado/Interfaces/IRepositorioBase.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace e_Agenda.WinApp.Compartilhado.Interfaces
+﻿namespace e_Agenda.WinApp.Compartilhado.Interfaces
 {
     public interface IRepositorioBase<IEntidadeBase>
     {
