67e64a521db2e63470ae9f4536ae9d72e62fa0f3
Davi Borges <davifelipe_300@hotmail.com>
2023-05-31 21:50:49 -0300

    adicionas os interfaces do modulos de tarefas na pasta de interfaces

---
+++ e-Agenda.WinApp/ModuloTarefa/Interfaces/IRepositorioTarefa.cs
@@ -0,0 +1,21 @@
+﻿using e_Agenda.WinApp.Compartilhado.Interfaces;
+using e_Agenda.WinApp.ModuloTarefa.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_Agenda.WinApp.ModuloTarefa.Interfaces
+{
+    public interface IRepositorioTarefa : IRepositorioBase<Tarefa>
+    {
+        void Inserir(Tarefa novaTarefa);
+        void Editar(int id, Tarefa tarefa);
+        void Excluir(Tarefa tarefaSelecionada);
+        Tarefa SelecionarPorId(int id);
+        List<Tarefa> SelecionarPendentes();
+        List<Tarefa> SelecionarConcluidas();
+        List<Tarefa> SelecionarTodosOrdenadosPorPrioridade();
+    }
+}
