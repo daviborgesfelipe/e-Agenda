53e9c4fb19dfe9813a02699d099555073aa8d704
Davi Borges <davifelipe_300@hotmail.com>
2023-05-31 21:45:35 -0300

    adiciona as interfaces do modulo de compromissos na pasta de interfaces

---
+++ e-Agenda.WinApp/ModuloCompromisso/Interfaces/IRepositorioCompromisso.cs
@@ -0,0 +1,15 @@
+﻿using e_Agenda.WinApp.Compartilhado.Interfaces;
+using e_Agenda.WinApp.ModuloCompromisso.Entidades;
+
+namespace e_Agenda.WinApp.ModuloCompromisso.Interfaces
+{
+    public interface IRepositorioCompromisso : IRepositorioBase<Compromisso>
+    {
+        void Inserir(Compromisso novoCompromisso);
+        void Editar(int id, Compromisso compromisso);
+        void Excluir(Compromisso compromissoSelecionada);
+        Compromisso SelecionarPorId(int id);
+        List<Compromisso> SelecionarCompromissosFuturos(DateTime dataInicio, DateTime dataTermino);
+        List<Compromisso> SelecionarCompromissosPassados(DateTime dateTime);
+    }
+}
