7c4d3b2bba4dfd087a07693c97108bda98042d9b
Davi Borges <davifelipe_300@hotmail.com>
2023-05-31 21:49:56 -0300

    adicionas os entidades do modulos de tarefas na pasta de entidades

---
+++ e-Agenda.WinApp/ModuloTarefa/Entidades/ItemTarefa.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_Agenda.WinApp.ModuloTarefa.Entidades
+{
+    public class ItemTarefa
+    {
+        public string titulo;
+        public bool concluido;
+
+        public ItemTarefa(string titulo)
+        {
+            this.titulo = titulo;
+        }
+
+        public override string ToString()
+        {
+            return titulo;
+        }
+
+        public void Concluir()
+        {
+            concluido = true;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is ItemTarefa tarefa &&
+                   titulo == tarefa.titulo &&
+                   concluido == tarefa.concluido;
+        }
+
+        public void Desmarcar()
+        {
+            concluido = false;
+        }
+    }
+}
---
+++ e-Agenda.WinApp/ModuloTarefa/Entidades/Tarefa.cs
@@ -0,0 +1,66 @@
+﻿using e_Agenda.WinApp.Compartilhado;
+using e_Agenda.WinApp.Compartilhado.Enums;
+
+namespace e_Agenda.WinApp.ModuloTarefa.Entidades
+{
+    [Serializable]
+    public class Tarefa : EntidadeBase<Tarefa>
+    {
+        public string titulo;
+        public TipoPrioridadeTarefaEnum prioridade;
+        public DateTime dataCriacao;
+        public List<ItemTarefa> items;
+        public decimal percentualConcluido;
+
+        public Tarefa(int id, string titulo, TipoPrioridadeTarefaEnum prioridade, DateTime dataCriacao)
+        {
+            this.id = id;
+            this.titulo = titulo;
+            this.prioridade = prioridade;
+            this.dataCriacao = dataCriacao;
+            items = new List<ItemTarefa>();
+        }
+
+        public override void AtualizarInformacoes(Tarefa registroAtualizado)
+        {
+            id = registroAtualizado.id;
+            titulo = registroAtualizado.titulo;
+            prioridade = registroAtualizado.prioridade;
+        }
+        public override string ToString()
+        {
+            return "Id: " + id + ", " + titulo + ", Prioridade: " + prioridade;
+        }
+        public override string[] Validar()
+        {
+            return new string[] { };
+        }
+        public void AdicionarItem(ItemTarefa item)
+        {
+            items.Add(item);
+        }
+        public void ConcluirItem(ItemTarefa item)
+        {
+            ItemTarefa itemSelecionado = items.FirstOrDefault(x => x.Equals(item));
+            itemSelecionado.Concluir();
+            CalcularPercentualConcluido();
+        }
+        private void CalcularPercentualConcluido()
+        {
+            decimal qtdItens = items.Count();
+            if (qtdItens == 0)
+            {
+                return;
+            }
+            decimal qtdConcluidos = items.Count(x => x.concluido == true);
+            decimal resultado = qtdConcluidos / qtdItens * 100;
+            percentualConcluido = Math.Round(resultado, 2);
+        }
+        public void DesmarcarItem(ItemTarefa item)
+        {
+            ItemTarefa itemSelecionado = items.FirstOrDefault(x => x.Equals(item));
+            itemSelecionado.Desmarcar();
+            CalcularPercentualConcluido();
+        }
+    }
+}
