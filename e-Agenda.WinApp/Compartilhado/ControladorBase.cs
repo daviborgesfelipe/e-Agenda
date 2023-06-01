53fe47e97758d9e2395dbce39b552c4a90ac6ee1
Davi Borges <davifelipe_300@hotmail.com>
2023-05-31 21:41:58 -0300

    adiciona tooltip e os metodos para adicionar e concluir um item

--- e-Agenda.WinApp/Compartilhado/ControladorBase.cs
+++ e-Agenda.WinApp/Compartilhado/ControladorBase.cs
@@ -8,8 +8,13 @@ namespace e_Agenda.WinApp.Compartilhado
         public abstract string ToolTipEditar { get; }
         public abstract string ToolTipExcluir { get; }
         public virtual string ToolTipFiltrar { get; }
+        public virtual string ToolTipAddItems { get; }
+        public virtual string ToolTipConcluirItemTarefa { get; }
+
 
         public virtual void Filtrar() { }
+        public virtual void Adicionar() { }
+        public virtual void ConcluirItens() { }
         public abstract void Inserir();
         public abstract void Editar();
         public abstract void Excluir();
