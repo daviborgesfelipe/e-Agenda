ce6fd47efaa1086982a63e04a453572e37317f56
Davi Borges <davifelipe_300@hotmail.com>
2023-05-31 21:50:37 -0300

    adicionas os interfaces do modulos de contatos na pasta de interfaces

---
+++ e-Agenda.WinApp/ModuloContato/Interfaces/IRepositorioContato.cs
@@ -0,0 +1,19 @@
+﻿using e_Agenda.WinApp.Compartilhado.Interfaces;
+using e_Agenda.WinApp.ModuloContato.Entidades;
+using e_Agenda.WinApp.ModuloTarefa.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_Agenda.WinApp.ModuloContato.Interfaces
+{
+    public interface IRepositorioContato : IRepositorioBase<Contato>
+    {
+        void Inserir(Contato novoContato);
+        void Editar(int id, Contato contato);
+        void Excluir(Contato contatoSelecionada);
+        Contato SelecionarPorId(int id);
+    }
+}
