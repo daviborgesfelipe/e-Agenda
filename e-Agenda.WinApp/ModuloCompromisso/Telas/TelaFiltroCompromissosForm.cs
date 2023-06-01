37df613973496d9159ca592322e795db0e71fe2b
Davi Borges <davifelipe_300@hotmail.com>
2023-05-31 21:46:51 -0300

    adicionas as telas do modulos de compromisso na pasta de telas

---
+++ e-Agenda.WinApp/ModuloCompromisso/Telas/TelaFiltroCompromissosForm.cs
@@ -0,0 +1,38 @@
+﻿namespace e_Agenda.WinApp.ModuloCompromisso
+{
+    public partial class TelaFiltroCompromissosForm : Form
+    {
+        public TelaFiltroCompromissosForm()
+        {
+            InitializeComponent();
+        }
+
+        public StatusCompromissoEnum StatusSelecionado
+        {
+            get
+            {
+                if (radioBtnCompromissosPassados.Checked)
+                {
+                    return StatusCompromissoEnum.Passados;
+                }
+                else if (radioBtnCompromissosFuturos.Checked)
+                {
+                    return StatusCompromissoEnum.Futuros;
+                }
+                else
+                {
+                    return StatusCompromissoEnum.Todos;
+                }
+            }
+        }
+
+        public DateTime DataInicial
+        {
+            get { return txtDataInicial.Value; }
+        }
+        public DateTime DataFinal
+        {
+            get { return txtDataFinal.Value; }
+        }
+    }
+}
