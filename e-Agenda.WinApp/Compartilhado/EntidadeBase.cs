9561f0a326b108d3bb12fa981227a7a2ef3be522
Davi Borges <davifelipe_300@hotmail.com>
2023-05-31 21:42:49 -0300

    altera o tipo do metodo validar de list para um array string

--- e-Agenda.WinApp/Compartilhado/EntidadeBase.cs
+++ e-Agenda.WinApp/Compartilhado/EntidadeBase.cs
@@ -1,10 +1,11 @@
 ﻿namespace e_Agenda.WinApp.Compartilhado
 {
+    [Serializable]
     public abstract class EntidadeBase<IEntidadeBase>
     {
         public int id;
         public abstract void AtualizarInformacoes(IEntidadeBase registroAtualizado);
-        public abstract List<string> Validar();
+        public abstract string[] Validar();
 
     }
 }
